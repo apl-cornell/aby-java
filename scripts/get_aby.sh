1ce3fdb22b86ed1fa1a9031264ce152f8d4dde0e
Cosku Acay <coskuacay@gmail.com>
2020-08-31 14:18:47 -0400

    Make get_aby.sh work even if there is an ABY directory

--- scripts/get_aby.sh
+++ scripts/get_aby.sh
@@ -7,17 +7,19 @@ set -e
 # shellcheck source=./variables.sh
 . "$(dirname "$0")"/variables.sh
 
+ABY_URL=https://github.com/encryptogroup/ABY
 echo "ABY Version: $ABY_VERSION"
 
 # Clone the ABY Git repository
-mkdir "$ABY_DIR"
+mkdir -p "$ABY_DIR"
 cd "$ABY_DIR"
 git init
-git remote add origin https://github.com/encryptogroup/ABY
-git fetch --quiet --depth 1 origin "$ABY_VERSION"
+git fetch --quiet --depth 1 $ABY_URL "$ABY_VERSION"
 git checkout --quiet "$ABY_VERSION"
+git reset --quiet --hard "$ABY_VERSION"
 git submodule update --init --recursive --depth 1 2> /dev/null
 
 # Remove C++ attributes since SWIG doesn't support them yet
-FILE=$ABY_DIR/src/abycore/circuit/circuit.h
-sed -i.bak 's/\[\[maybe_unused\]\]//g' "$FILE" && rm "$FILE.bak"
+FILE=src/abycore/circuit/circuit.h
+echo "Removing C++ attributes in $FILE"
+sed -i.bak 's/\[\[maybe_unused\]\]//g' "$ABY_DIR/$FILE" && rm "$ABY_DIR/$FILE.bak"
