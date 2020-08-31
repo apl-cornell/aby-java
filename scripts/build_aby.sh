531fd84174c4b9bfc985925e644b81855aa96d1c
Cosku Acay <coskuacay@gmail.com>
2020-08-30 21:05:27 -0400

    Remove C++ attributes from headers since SWIG doesn't support them

--- scripts/build_aby.sh
+++ scripts/build_aby.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+INSTALL_DIR=$(pwd)/ABY/build/install
+
 # TODO: remove, since these only apply to MacOS
 OPENSSL_ROOT_DIR=$(brew --prefix openssl)
 export OPENSSL_ROOT_DIR
@@ -9,4 +11,7 @@ export OPENSSL_ROOT_DIR
 cd ABY
 cmake -B build
 cmake --build build
-cmake --install build --prefix "$(pwd)/build/install"
+cmake --install build --prefix "$INSTALL_DIR"
+
+# Remove C++ attributes since SWIG doesn't support them yet
+sed -i.bak 's/\[\[maybe_unused\]\]//g' "$INSTALL_DIR/include/abycore/circuit/circuit.h"
