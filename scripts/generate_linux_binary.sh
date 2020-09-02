e97f1ef24a4733febcf195a71dcb1a630fc80de3
Cosku Acay <coskuacay@gmail.com>
2020-09-02 18:41:13 -0400

    Add script to copy the Linux binary out of the Docker container

---
+++ scripts/generate_linux_binary.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Builds the Linux binary using Docker and copies it into the correct directory.
+
+set -e
+
+# Build the Docker image and remember its ID.
+IMAGE_ID=$(docker build . | tee /dev/tty | tail -n1 | cut -d' ' -f3)
+
+# Copy the linux binary out.
+OUTPUT_DIR=src/main/resources/natives/linux_64
+mkdir -p $OUTPUT_DIR
+
+CONTAINER_ID=$(docker create "$IMAGE_ID")
+docker cp "$CONTAINER_ID:/root/build/cmake/install/lib/libabyjava.so" $OUTPUT_DIR/
+docker rm "$CONTAINER_ID"
