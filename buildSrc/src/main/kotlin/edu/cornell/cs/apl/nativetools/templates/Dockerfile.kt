82431b1967a484c271181f61315ab056a0c28c49
Cosku Acay <coskuacay@gmail.com>
2021-02-22 23:46:07 -0500

    Fix regression in handling of environment variables

--- buildSrc/src/main/kotlin/edu/cornell/cs/apl/nativetools/templates/Dockerfile.kt
+++ buildSrc/src/main/kotlin/edu/cornell/cs/apl/nativetools/templates/Dockerfile.kt
@@ -36,7 +36,7 @@ internal val linuxDockerfile = Platform.LINUX_64.let { platform ->
     CMD ["/bin/bash"]
 
     ## Configure Conan
-    ENV PATH="/opt/python/cp35-cp35m/bin:${'$'}PATH"
+    ENV PATH="/opt/python/cp35-cp35m/bin:${'$'}{PATH}"
     RUN conan profile new --detect default \
         && conan profile update settings.compiler.libcxx=libstdc++11 default
 
