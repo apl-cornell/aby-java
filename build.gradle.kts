8113f7cd837b7d3928b88dabb60cd721a3407fe2
Cosku Acay <coskuacay@gmail.com>
2021-09-22 20:33:19 -0400

    Update Jacoco config

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ tasks.test {
 
 tasks.jacocoTestReport {
     reports {
-        xml.isEnabled = true
-        html.isEnabled = true
+        xml.required.set(true)
+        html.required.set(true)
     }
     dependsOn(tasks.test)
 }
