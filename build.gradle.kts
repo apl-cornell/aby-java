07c34d82c6fa06a82d87c73952d619ac3db386e0
Cosku Acay <coskuacay@gmail.com>
2021-09-22 20:32:11 -0400

    Remove unnecessary version numbers for JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ java {
 dependencies {
     // Testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
     // Logging during testing
     testImplementation("org.slf4j:slf4j-simple:1.7.32")
