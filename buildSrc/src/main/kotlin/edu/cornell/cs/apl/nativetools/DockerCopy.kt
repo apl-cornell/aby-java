79577b25134b9560e7e19027d927f76cec91f09a
Cosku Acay <coskuacay@gmail.com>
2021-10-06 14:52:33 -0400

    Use BuildKit when building Docker images

--- buildSrc/src/main/kotlin/edu/cornell/cs/apl/nativetools/DockerCopy.kt
+++ buildSrc/src/main/kotlin/edu/cornell/cs/apl/nativetools/DockerCopy.kt
@@ -1,10 +1,10 @@
 package edu.cornell.cs.apl.nativetools
 
-import java.io.ByteArrayOutputStream
-import java.io.File
 import org.apache.tools.ant.util.TeeOutputStream
 import org.gradle.api.Project
 import org.gradle.api.file.RegularFile
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 /** Builds a Docker image and copies files from the image to local host. */
 internal fun Project.dockerCopy(
@@ -13,21 +13,25 @@ internal fun Project.dockerCopy(
     dockerfile: RegularFile,
     target: String
 ) {
-    fun docker(vararg arguments: String): String {
-        ByteArrayOutputStream().use { os ->
-            exec {
-                executable = "docker"
-                args = arguments.toList()
-                standardOutput = TeeOutputStream(this.standardOutput, os)
+    fun docker(vararg arguments: String): Pair<String, String> {
+        ByteArrayOutputStream().use { stdout ->
+            ByteArrayOutputStream().use { stderr ->
+                exec {
+                    executable = "docker"
+                    args = arguments.toList()
+                    standardOutput = TeeOutputStream(this.standardOutput, stdout)
+                    errorOutput = TeeOutputStream(this.errorOutput, stderr)
+                }
+                return Pair(stdout.toString(), stderr.toString())
             }
-            return os.toString()
         }
     }
 
     val imageID =
-        docker("build", "--file", "$dockerfile", "--target", target, dockerfile.asFile.parent)
-            .lines().dropLast(1).last().trim().split(" ")[2]
-    val containerID = docker("create", imageID).trim()
+        docker("buildx", "build", "--file", "$dockerfile", "--target", target, dockerfile.asFile.parent).second.let {
+            Regex("sha256:[0-9a-f]*").findAll(it).last().value
+        }
+    val containerID = docker("create", imageID).first.trim()
 
     try {
         project.delete(to)
