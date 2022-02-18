77788ff883ab4e478a1ca30358392a58b954c0b3
Josh Acay <coskuacay@gmail.com>
2022-02-17 19:23:04 -0500

    Add Maven Central publishing support (#81)
    * Add Maven Central publishing support
    
    * Dry run publish task
    
    * Fetch history when building
    
    * Always sign artifacts if a key is available
    
    * Automatically close Sonatype staging repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,11 @@ import org.gradle.api.JavaVersion.VERSION_1_8
 
 plugins {
     `java-library`
+
+    // Publishing
     `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 
     // Versioning
     id("pl.allegro.tech.build.axion-release") version "1.13.6"
@@ -29,6 +33,9 @@ version = scmVersion.version
 java {
     sourceCompatibility = VERSION_1_8
     targetCompatibility = VERSION_1_8
+
+    withJavadocJar()
+    withSourcesJar()
 }
 
 dependencies {
@@ -71,27 +78,6 @@ tasks.jacocoTestReport {
     dependsOn(tasks.test)
 }
 
-/** Publishing */
-
-publishing {
-    publications {
-        create<MavenPublication>("default") {
-            from(components["java"])
-        }
-    }
-
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
-
 /** Building Native Binaries */
 
 swigLibrary {
@@ -109,4 +95,72 @@ swigLibrary {
             includeDirectories = listOf("src", "extern/ENCRYPTO_utils/src")
         )
     )
+}
+
+/** Publishing */
+
+publishing {
+    val repository = System.getenv("GITHUB_REPOSITORY") ?: "apl-cornell/aby-java"
+    val repositoryUrl = "github.com/$repository"
+
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+
+            pom {
+                name.set("$groupId.$artifactId")
+                description.set("Java bindings for the ABY framework.")
+                url.set("https://$repositoryUrl")
+
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public License v3.0")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0.en.html")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("Coşku Acay")
+                        email.set("coskuacay@gmail.com")
+                        url.set("https://github.com/cacay")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://$repositoryUrl.git")
+                    developerConnection.set("scm:git:ssh://$repositoryUrl.git")
+                    url.set("https://$repositoryUrl")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.$repositoryUrl")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    isRequired = signingKey != null
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
 }
